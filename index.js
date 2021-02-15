09ad056dee35dbfa2ce31aa88cd4676413e19d28
lucas-lmk <lucas@lmksystem.fr>
2021-02-16 00:52:37 +0100

    default server created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Has Started on port " + process.env.PORT);
+});
